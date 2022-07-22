4b75e5e7889492fe942ccc3ac8a1edf25b88d090
SunilPrabakar143 <sprupgraded@gmail.com>
2022-07-22 10:43:16 +0530

    new code

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,25 @@
+package org.computer;
+
+public class Desktop implements HardWare, Software {
+
+	private void destop() {
+		System.out.println("my desktop ");
+	}
+
+	@Override
+	public void softwareResources() {
+		System.out.println(" softwareResources are 1234 ");
+	}
+
+	@Override
+	public void hardwareResources() {
+		System.out.println(" hardwareResources are 5678 ");
+	}
+
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.destop();
+		d.hardwareResources();
+		d.softwareResources();
+	}
+}
